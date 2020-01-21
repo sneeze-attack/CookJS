aac2ec55cb26be47dcd18295bb5f786d31906ba5
sneeze-attack <1347409+sneeze-attack@users.noreply.github.com>
2020-01-21 15:24:29 -0500

    Set up volume conversion

--- src/modules/Inventory.js
+++ src/modules/Inventory.js
@@ -5,9 +5,69 @@ export default class Inventory {
 
     // all measurements here start at tsp, the most common smallest measurement
     // of volume in English units
-    // We will convert up to other units everywhere else for display / UI
+    // We will convert() up to other units everywhere else for display / UI
 
-    // Start with a pound of flour which is about 3 1/2 cups
+    // Start with a pound of flour which is about 3 1/2 cups (168tsp)
     this.flour = 168;
+  }
+
+  convert(number) {
+    // gallon conversion
+    const gallons = Math.floor(number / 768);
+    let remainingGallons;
+    if (gallons === 0) {
+      remainingGallons = '';
+    } else if (gallons === 1) {
+      remainingGallons = `${gallons} gallon, `;
+    } else {
+      remainingGallons = `${gallons} gallons, `;
+    }
+    // quart conversion
+    let remainder = number % 768;
+    let quarts = Math.floor(remainder / 192);
+    let remainingQuarts;
+    if (quarts === 0) {
+      remainingQuarts = '';
+    } else if (quarts === 1) {
+      remainingQuarts = `${quarts} quart, `;
+    } else {
+      remainingQuarts = `${quarts} quarts, `;
+    }
+    // pint conversion
+    let quartRemainder = remainder % 192;
+    let pints = Math.floor(quartRemainder / 96);
+    let remainingPints = '';
+    if (pints !== 0) {
+      remainingPints = `${pints} pint, `;
+    }
+
+    // cup conversion
+    let pintRemainder = quartRemainder % 96;
+    let cups = Math.floor(pintRemainder / 48);
+    let remainingCups = '';
+    if (cups !== 0) {
+      remainingCups = `${cups} cup, `;
+    }
+
+    // tablespoon conversion
+    let cupRemainder = pintRemainder % 48;
+    let tablespoons = Math.floor(cupRemainder / 3);
+    let remainingTablespoons;
+    if (tablespoons === 0) {
+      remainingTablespoons = '';
+    } else {
+      remainingTablespoons = `${tablespoons} Tbsp, `;
+    }
+
+    // teaspoon conversion
+    let teaspoons = cupRemainder % 3;
+    let remainingTeaspoons;
+    if (teaspoons === 0) {
+      remainingTeaspoons = '';
+    } else {
+      remainingTeaspoons = `${teaspoons} tsp`
+    }
+
+    return remainingGallons + remainingQuarts + remainingPints + remainingCups + remainingTablespoons + remainingTeaspoons;
   }
 }
--- src/modules/ui/Main.js
+++ src/modules/ui/Main.js
@@ -9,7 +9,7 @@ export default class Main extends Phaser.GameObjects.Group {
     scene.add.image(0, 0, 'paperBackground').setOrigin(0, 0);
     // display inventory
     scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 116) / 128), `Cash: $${game.inventory.dollars}`).setColor('#000000').setInteractive().setFontSize(32).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
-    scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 110) / 128), `Flour: ${game.inventory.flour} tsp`).setColor('#000000').setInteractive().setFontSize(32).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
+    scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 110) / 128), `Flour: ${game.inventory.convert(`${game.inventory.flour}`)}`).setColor('#000000').setInteractive().setFontSize(32).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
     super();
   }
 }
