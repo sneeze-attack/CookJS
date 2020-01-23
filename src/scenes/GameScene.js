cba4b45f062a982030da883660da18ff27430e77
sneeze-attack <1347409+sneeze-attack@users.noreply.github.com>
2020-01-23 14:36:27 -0500

    Combined functions into single reusable one, added formatting for texts

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -14,94 +14,71 @@ export default class GameScene extends Phaser.Scene {
     // use object to set up UI
     const ui = new Main(this);
 
-    // Add 5 lb flour and reduce cash on click
-    function buyFlour() {
-      if (game.inventory.dollars > 2.99) {
-        game.inventory.flour += 840;
-        ui.flourHeld.setText(`Flour: ${game.inventory.convert(`${game.inventory.flour}`)}`);
-        game.inventory.dollars -= 2.99;
-        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
+    function buy(ingredient, price, tsp) {
+      if (game.inventory.dollars > price) {
+        game.inventory[ingredient] += tsp;
+        // capitalize name of ingredient
+        const ingredientCapitalized = ingredient.charAt(0).toUpperCase() + ingredient.slice(1);
+        // add blank space to align with longest ingredient name
+        let spacer = ''
+        const l = ingredient.length;
+        // MODIFY THIS MANUALLY FOR NOW, ADJUST WHEN NEW INGREDIENTS ARE ADDED
+        const longestIngredientLength = 5;
+        if (l < longestIngredientLength) {
+          const d = longestIngredientLength - l;
+          if (d === 1) {
+            spacer = ' ';
+          } else if (d === 2) {
+            spacer = '  ';
+          } else if (d === 3) {
+            spacer = '   ';
+          } else {
+            spacer = '    ';
+          }
+        }
+        // update ingredient text to show new amount held
+        ui[ingredient + 'Held'].setText(`${ingredientCapitalized}:${spacer} ${game.inventory.convert(`${game.inventory[ingredient]}`)}`);
+        game.inventory.dollars -= price;
+        // subtracting repeatedly, .000000001 remainder - use 'toFixed(2)'
         const rounded = game.inventory.dollars.toFixed(2);
         ui.cashHeld.setText(`Cash: $${rounded}`);
       }
     }
+
+    // Add 5 lb and reduce cash held by $2.99 on click
     ui.buyFlour.on('pointerup', () => {
-      buyFlour.call(this);
+      buy('flour', 2.99, 840);
     });
     ui.buyFlourText.on('pointerup', () => {
-      buyFlour.call(this);
+      buy('flour', 2.99, 840);
     });
-
-    // sugar
-    function buySugar() {
-      if (game.inventory.dollars > 2.99) {
-        game.inventory.sugar += 384;
-        ui.sugarHeld.setText(`Sugar: ${game.inventory.convert(`${game.inventory.sugar}`)}`);
-        game.inventory.dollars -= 2.99;
-        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-        const rounded = game.inventory.dollars.toFixed(2);
-        ui.cashHeld.setText(`Cash: $${rounded}`);
-      }
-    }
+    // Add 4 lb and reduce cash held by $2.99 on click
     ui.buySugar.on('pointerup', () => {
-      buySugar.call(this);
+      buy('sugar', 2.99, 384);
     });
     ui.buySugarText.on('pointerup', () => {
-      buySugar.call(this);
+      buy('sugar', 2.99, 384);
     });
-
-    // yeast
-    function buyYeast() {
-      if (game.inventory.dollars > 13.99) {
-        game.inventory.yeast += 160;
-        ui.yeastHeld.setText(`Yeast: ${game.inventory.convert(`${game.inventory.yeast}`)}`);
-        game.inventory.dollars -= 13.99;
-        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-        const rounded = game.inventory.dollars.toFixed(2);
-        ui.cashHeld.setText(`Cash: $${rounded}`);
-      }
-    }
+    // Add 1 lb and reduce cash held by $13.99 on click
     ui.buyYeast.on('pointerup', () => {
-      buyYeast.call(this);
+      buy('yeast', 13.99, 160);
     });
     ui.buyYeastText.on('pointerup', () => {
-      buyYeast.call(this);
+      buy('yeast', 13.99, 160);
     });
-
-    // salt
-    function buySalt() {
-      if (game.inventory.dollars > 3.69) {
-        game.inventory.salt += 240;
-        ui.saltHeld.setText(`Salt:  ${game.inventory.convert(`${game.inventory.salt}`)}`);
-        game.inventory.dollars -= 3.69;
-        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-        const rounded = game.inventory.dollars.toFixed(2);
-        ui.cashHeld.setText(`Cash: $${rounded}`);
-      }
-    }
+    // Add 3 lb and reduce cash held by $3.69 on click
     ui.buySalt.on('pointerup', () => {
-      buySalt.call(this);
+      buy('salt', 3.69, 240);
     });
     ui.buySaltText.on('pointerup', () => {
-      buySalt.call(this);
+      buy('salt', 3.69, 240);
     });
-
-    // oil
-    function buyOil() {
-      if (game.inventory.dollars > 4.59) {
-        game.inventory.oil += 768;
-        ui.oilHeld.setText(`Oil:   ${game.inventory.convert(`${game.inventory.oil}`)}`);
-        game.inventory.dollars -= 4.59;
-        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-        const rounded = game.inventory.dollars.toFixed(2);
-        ui.cashHeld.setText(`Cash: $${rounded}`);
-      }
-    }
+    // Add 1 gallon and reduce cash held by $4.59 on click
     ui.buyOil.on('pointerup', () => {
-      buyOil.call(this);
+      buy('oil', 4.59, 768);
     });
     ui.buyOilText.on('pointerup', () => {
-      buyOil.call(this);
+      buy('oil', 4.59, 768);
     });
   }
 }
