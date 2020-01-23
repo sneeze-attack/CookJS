fb11986810fae051a91e10163bb8444d7cb71e1a
sneeze-attack <1347409+sneeze-attack@users.noreply.github.com>
2020-01-23 11:14:06 -0500

    Lint

--- src/modules/GameState.js
+++ src/modules/GameState.js
@@ -1,12 +1,11 @@
 export default class GameState {
-	constructor() {
-		this.nextScene = 'StartScene';
-		this.previousScene = null;
-	}
+  constructor() {
+    this.nextScene = 'StartScene';
+    this.previousScene = null;
+  }
 
-	changeScene(nextScene, thisScene) {
-		this.nextScene = nextScene;
-		this.previousScene = thisScene;
-	}
-
+  changeScene(nextScene, thisScene) {
+    this.nextScene = nextScene;
+    this.previousScene = thisScene;
+  }
 }
