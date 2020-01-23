c1b32aa87a0645815a776626a122a22f9166c2bd
sneeze-attack <1347409+sneeze-attack@users.noreply.github.com>
2020-01-23 11:40:33 -0500

    Prevent purchases if not enough money

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -16,12 +16,14 @@ export default class GameScene extends Phaser.Scene {
 
     // Add 5 lb flour and reduce cash on click
     function buyFlour() {
-      game.inventory.flour += 840;
-      ui.flourHeld.setText(`Flour: ${game.inventory.convert(`${game.inventory.flour}`)}`);
-      game.inventory.dollars -= 2.99;
-      // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-      const rounded = game.inventory.dollars.toFixed(2);
-      ui.cashHeld.setText(`Cash: $${rounded}`);
+      if (game.inventory.dollars > 2.99) {
+        game.inventory.flour += 840;
+        ui.flourHeld.setText(`Flour: ${game.inventory.convert(`${game.inventory.flour}`)}`);
+        game.inventory.dollars -= 2.99;
+        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
+        const rounded = game.inventory.dollars.toFixed(2);
+        ui.cashHeld.setText(`Cash: $${rounded}`);
+      }
     }
     ui.buyFlour.on('pointerup', () => {
       buyFlour.call(this);
@@ -32,12 +34,14 @@ export default class GameScene extends Phaser.Scene {
 
     // sugar
     function buySugar() {
-      game.inventory.sugar += 384;
-      ui.sugarHeld.setText(`Sugar: ${game.inventory.convert(`${game.inventory.sugar}`)}`);
-      game.inventory.dollars -= 2.99;
-      // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-      const rounded = game.inventory.dollars.toFixed(2);
-      ui.cashHeld.setText(`Cash: $${rounded}`);
+      if (game.inventory.dollars > 2.99) {
+        game.inventory.sugar += 384;
+        ui.sugarHeld.setText(`Sugar: ${game.inventory.convert(`${game.inventory.sugar}`)}`);
+        game.inventory.dollars -= 2.99;
+        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
+        const rounded = game.inventory.dollars.toFixed(2);
+        ui.cashHeld.setText(`Cash: $${rounded}`);
+      }
     }
     ui.buySugar.on('pointerup', () => {
       buySugar.call(this);
@@ -48,12 +52,14 @@ export default class GameScene extends Phaser.Scene {
 
     // yeast
     function buyYeast() {
-      game.inventory.yeast += 160;
-      ui.yeastHeld.setText(`Yeast: ${game.inventory.convert(`${game.inventory.yeast}`)}`);
-      game.inventory.dollars -= 13.99;
-      // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-      const rounded = game.inventory.dollars.toFixed(2);
-      ui.cashHeld.setText(`Cash: $${rounded}`);
+      if (game.inventory.dollars > 13.99) {
+        game.inventory.yeast += 160;
+        ui.yeastHeld.setText(`Yeast: ${game.inventory.convert(`${game.inventory.yeast}`)}`);
+        game.inventory.dollars -= 13.99;
+        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
+        const rounded = game.inventory.dollars.toFixed(2);
+        ui.cashHeld.setText(`Cash: $${rounded}`);
+      }
     }
     ui.buyYeast.on('pointerup', () => {
       buyYeast.call(this);
@@ -64,12 +70,14 @@ export default class GameScene extends Phaser.Scene {
 
     // salt
     function buySalt() {
-      game.inventory.salt += 240;
-      ui.saltHeld.setText(`Salt:  ${game.inventory.convert(`${game.inventory.salt}`)}`);
-      game.inventory.dollars -= 3.69;
-      // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-      const rounded = game.inventory.dollars.toFixed(2);
-      ui.cashHeld.setText(`Cash: $${rounded}`);
+      if (game.inventory.dollars > 3.69) {
+        game.inventory.salt += 240;
+        ui.saltHeld.setText(`Salt:  ${game.inventory.convert(`${game.inventory.salt}`)}`);
+        game.inventory.dollars -= 3.69;
+        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
+        const rounded = game.inventory.dollars.toFixed(2);
+        ui.cashHeld.setText(`Cash: $${rounded}`);
+      }
     }
     ui.buySalt.on('pointerup', () => {
       buySalt.call(this);
@@ -80,12 +88,14 @@ export default class GameScene extends Phaser.Scene {
 
     // oil
     function buyOil() {
-      game.inventory.oil += 768;
-      ui.oilHeld.setText(`Oil:   ${game.inventory.convert(`${game.inventory.oil}`)}`);
-      game.inventory.dollars -= 4.59;
-      // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
-      const rounded = game.inventory.dollars.toFixed(2);
-      ui.cashHeld.setText(`Cash: $${rounded}`);
+      if (game.inventory.dollars > 4.59) {
+        game.inventory.oil += 768;
+        ui.oilHeld.setText(`Oil:   ${game.inventory.convert(`${game.inventory.oil}`)}`);
+        game.inventory.dollars -= 4.59;
+        // subtracting repeatedly, left w/ .000000001 - need 'toFixed(2)'
+        const rounded = game.inventory.dollars.toFixed(2);
+        ui.cashHeld.setText(`Cash: $${rounded}`);
+      }
     }
     ui.buyOil.on('pointerup', () => {
       buyOil.call(this);
