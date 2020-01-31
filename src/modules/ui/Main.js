3d10a956ab4cf7ea61a2910589a53f074aefe10f
sneeze-attack <1347409+sneeze-attack@users.noreply.github.com>
2020-01-31 11:48:12 -0500

    Code refactor

--- src/modules/ui/Main.js
+++ src/modules/ui/Main.js
@@ -9,24 +9,23 @@ export default class Main extends Phaser.GameObjects.Group {
     // add background
     scene.add.image(0, 0, 'paperBackground').setOrigin(0, 0);
 
-    // create purchase buttons and their text
+    // create purchase buttons and their text, create inventory display
     let heightCoord = 12;
     let textHeightCoord = 13;
+    let heldCoord = 90;
     Object.keys(jsonData).forEach((key) => {
       const tempName = `buy${key}`;
       const tempTextName = `buy${key}Text`;
+      const heldName = `${key}Held`;
+      // creates buttons and text in upper right
       window[tempName] = scene.add.rectangle(((config.scale.width * 89) / 128), ((config.scale.height * heightCoord) / 128), ((config.scale.width * 38) / 128), ((config.scale.height * 8) / 128), 0xC0C0C0).setOrigin(0, 0).setInteractive().setDepth(1);
       window[tempTextName] = scene.add.text(((config.scale.width * 90) / 128), ((config.scale.height * textHeightCoord) / 128), `Buy ${key} ${jsonData[key].price} / ${jsonData[key].amountText}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
+      // this creates inventory in bottom left
+      window[heldName] = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * heldCoord) / 128), `${key}: ${game.inventory.convert(`${game.inventory[key]}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
       heightCoord += 12;
       textHeightCoord += 12;
+      heldCoord += 5;
     });
-
-    // display inventory
-    const oilHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 90) / 128), `Oil:   ${game.inventory.convert(`${game.inventory.oil}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
-    const saltHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 95) / 128), `Salt:  ${game.inventory.convert(`${game.inventory.salt}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
-    const yeastHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 100) / 128), `Yeast: ${game.inventory.convert(`${game.inventory.yeast}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
-    const sugarHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 105) / 128), `Sugar: ${game.inventory.convert(`${game.inventory.sugar}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
-    const flourHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 110) / 128), `Flour: ${game.inventory.convert(`${game.inventory.flour}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
     const cashHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 116) / 128), `Cash: $${game.inventory.dollars}`).setColor('#000000').setInteractive().setFontSize(32).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
 
     // display day / time
