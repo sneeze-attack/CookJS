b103669b58162b4ed4aefd59a13911f056378893
sneeze-attack <1347409+sneeze-attack@users.noreply.github.com>
2020-02-24 10:41:47 -0500

    Add Banana Muffin recipe

--- src/modules/ui/Main.js
+++ src/modules/ui/Main.js
@@ -13,28 +13,31 @@ export default class Main extends Phaser.GameObjects.Group {
     // create oven
     const oven = scene.add.sprite(((config.scale.width * 3) / 128), ((config.scale.height * 4) / 128), 'ovenIcon').setOrigin(0, 0).setInteractive().setDepth(1);
 
-    // cooking menu
+    // oven / cooking menu
     // add gray shade to entire screen
     const menuShadeBox = scene.add.rectangle(0, 0, config.scale.width, config.scale.height, 0x000000).setOrigin(0, 0).setDepth(0).setAlpha(0.5).setInteractive();
     // add bordered results box
     const menuBoxBorder = scene.add.rectangle(((config.scale.width * 30) / 128), ((config.scale.height * 14) / 128), ((config.scale.width * 68) / 128), ((config.scale.height * 98) / 128), 0xFFFFFF).setOrigin(0, 0).setDepth(0).setInteractive();
     const menuBox = scene.add.rectangle(((config.scale.width * 30.25) / 128), ((config.scale.height * 14.5) / 128), ((config.scale.width * 67.5) / 128), ((config.scale.height * 97) / 128), 0x000000).setOrigin(0, 0).setDepth(0).setInteractive();
     // Show available recipes
-    let recipeHeightCoord = 12;
-    let recipeTextHeightCoord = 13;
+    let recipeHeightCoord = 17;
+    let recipeTextHeightCoord = 18;
+    // create a button for each recipe
     Object.keys(recipeData).forEach((key) => {
       console.log(key); // breadloaf
       const tempName = `${key}RecipeButton`;
       const tempTextName = `${key}RecipeButtonText`;
       // creates buttons and text in upper right
-      window[tempName] = scene.add.rectangle(((config.scale.width * 89) / 128), ((config.scale.height * recipeHeightCoord) / 128), ((config.scale.width * 38) / 128), ((config.scale.height * 8) / 128), 0xC0C0C0).setOrigin(0, 0).setInteractive().setDepth(0);
-      window[tempTextName] = scene.add.text(((config.scale.width * 90) / 128), ((config.scale.height * recipeTextHeightCoord) / 128), `${key}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(0);
+      window[tempName] = scene.add.rectangle(((config.scale.width * 32) / 128), ((config.scale.height * recipeHeightCoord) / 128), ((config.scale.width * 38) / 128), ((config.scale.height * 8) / 128), 0xC0C0C0).setOrigin(0, 0).setInteractive().setDepth(0);
+      window[tempTextName] = scene.add.text(((config.scale.width * 33) / 128), ((config.scale.height * recipeTextHeightCoord) / 128), `${recipeData[key].name}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(0);
+      recipeHeightCoord += 12;
+      recipeTextHeightCoord += 12;
     });
 
     // create purchase buttons and their text, create inventory display
     let heightCoord = 12;
     let textHeightCoord = 13;
-    let heldCoord = 90;
+    let heldCoord = 70;
     // Get length of longest ingredient
     let longestIngredientLength = 0;
     Object.keys(jsonData).forEach((key) => {
@@ -47,7 +50,8 @@ export default class Main extends Phaser.GameObjects.Group {
       const tempTextName = `buy${key}Text`;
       const heldName = `${key}Held`;
       // Capitalize first letter of ingredient
-      const ingredientCapitalized = key.charAt(0).toUpperCase() + key.slice(1);
+      const cap = `${jsonData[key].name}`;
+      const ingredientCapitalized = cap.charAt(0).toUpperCase() + cap.slice(1);
       // Add spacing for ingredients/inventory
       let spacer = '';
       const l = key.length;
@@ -56,15 +60,17 @@ export default class Main extends Phaser.GameObjects.Group {
           spacer = spacer.concat(' ');
         }
       }
-      // creates buttons and text in upper right
+      // creates purchase buttons and text in upper right
       window[tempName] = scene.add.rectangle(((config.scale.width * 89) / 128), ((config.scale.height * heightCoord) / 128), ((config.scale.width * 38) / 128), ((config.scale.height * 8) / 128), 0xC0C0C0).setOrigin(0, 0).setInteractive().setDepth(1);
-      window[tempTextName] = scene.add.text(((config.scale.width * 90) / 128), ((config.scale.height * textHeightCoord) / 128), `Buy ${key} $${jsonData[key].price} / ${jsonData[key].amountText}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
+      window[tempTextName] = scene.add.text(((config.scale.width * 90) / 128), ((config.scale.height * textHeightCoord) / 128), `${ingredientCapitalized} $${jsonData[key].price} / ${jsonData[key].amountText}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
       // this creates inventory in bottom left
       window[heldName] = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * heldCoord) / 128), `${ingredientCapitalized}:${spacer} ${game.inventory.convert(`${game.inventory[key]}`)}`).setColor('#000000').setInteractive().setFontSize(28).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
       heightCoord += 12;
       textHeightCoord += 12;
       heldCoord += 5;
     });
+
+    // display money held
     const cashHeld = scene.add.text(((config.scale.width * 8) / 128), ((config.scale.height * 116) / 128), `Cash: $${game.inventory.dollars}`).setColor('#000000').setInteractive().setFontSize(32).setFontFamily('"DejaVu Sans Mono"').setDepth(1);
 
     // display day / time
